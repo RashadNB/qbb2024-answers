a8a78c8bf5e48331a6adeee6deb1ab74f8568a32
RashadNB <rbatley1@jh.edu>
2024-10-02 19:30:19 -0400

    Part 1.3 completed

--- week3/ex1.sh
+++ week3/ex1.sh
@@ -16,3 +16,13 @@ expr 2678192 / 4  # 669548
 # There are 669548 reads in the file.
 
 
+### Question 1.3 ###
+# find size of reference genome by grepping for lines that start with > which are the sequences.
+grep -v '^>' sacCer3.fa | tr -d '\n' | wc -c
+
+# reference genome is 12157105 bp long.
+
+# expected average depth of coverage genome length divided by reads
+echo "76 * 669548 / 12200000" | bc 
+# the depth of coverage is 4
+

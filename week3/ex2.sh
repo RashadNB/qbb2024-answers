2cd22af3ed8ab83dca91cda1e7399fa7a398359c
RashadNB <rbatley1@jh.edu>
2024-10-03 08:27:16 -0400

    Finished part 2.2

--- week3/ex2.sh
+++ week3/ex2.sh
@@ -4,3 +4,13 @@
 grep -c "^>" sacCer3.fa # grep for > because chromosomes start with > in FASTA files.
 # There are 17 chromosomes in the yeast genome (16 plus one mitochondrial).
 
+### Question 2.2 ###
+for my_sample in A01_*.fastq # align reads to reference genome by extracting the basename in each seq file, align it to the reference, and put it in a sam file.
+do
+    echo ${my_sample}
+    my_sample=$(basename ${my_sample} .fastq)
+    bwa mem -t 4 -R "@RG\tID:${my_sample}\tSM:${my_sample}" sacCer3.fa ${my_sample}.fastq > ${my_sample}.sam
+done
+less -S A01_09.sam
+grep -v "^@" A01_09.sam | wc -l 
+# There are 669548 read alignments in the file.

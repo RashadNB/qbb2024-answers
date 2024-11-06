6b688e769b3044db8a5f7d4ff74f3f8b153844cc
RashadNB <rbatley1@jh.edu>
2024-11-06 11:09:30 -0500

    Finished Q 1.2 and UMAP

--- week8/week8.R
+++ week8/week8.R
@@ -10,9 +10,20 @@ gut = readH5AD("v2_fca_biohub_gut_10x_raw.h5ad")
 assayNames(gut) = "counts"
 gut = logNormCounts(gut)
 
-#Question 1
+#Question 1.1
 gut
 # There are 13407 genes.
 # They analyzed 11788 cells.
 # The dimension reduction datasets present are PCA, TSNE, and UMAP
+
+#Question 1.2
+colData(gut)
+# There are 39 columns
+colnames(colData(gut))
+
+# I find the most interesting columns to me would be age, tissue, and log_n_counts.
+# Together they could give some interesting insight into the fly aging process.
+
+set.seed(88)
+plotReducedDim(gut,"X_umap",color="broad_annotation")
 
